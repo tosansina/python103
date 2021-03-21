2d781d1c01fce4413ff51bc40dd704af5b6795de
sina <sinatosan@gmail.com>
2021-03-21 17:09:24 +0330

    advanced begin

---
+++ Advanced.py
@@ -0,0 +1,17 @@
+A = [0,1,'two'] # Created list  // list() // mutable-ordered
+B = (3,4,'Five') # created Tuple // tuple() // mutable-ordered
+C = {'target1':'python','target2':'Image_Processing'} #Created Dictionary  //dict() // mutable - unordered
+D = {6,7,'seven'} # Created set  // set() // immutable - unordered
+#print (type(A),type(B),type(C),type(D))
+#//////////////////////////////////////////////////////////////////////////////////////
+a = 1
+b = 2.5 
+c = '3'
+#print ("value a = %i, value b = %f, value c = %c"  % (a,b,c))
+#/////////////////////////////////////////////////////////////////////////////////////////////
+var = 0,1,2,3,4,5,6,0,7,8,9,0,0,7,100
+List = [5 for i in var]
+print(List)
+List1 = [i for i in var if i !=0]
+print(List1)
+#////////////////////////////////////////////////////////////////////////////////////
